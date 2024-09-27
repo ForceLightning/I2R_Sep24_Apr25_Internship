85b961dd60bb4aa264884e7fd1082de524c50283
Christopher Kok <ForceLightning@users.noreply.github.com>
2024-09-27 09:41:07 +0800

    test(models/attention): add tests for res-attention unet

--- tests/quick_test.py
+++ tests/quick_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Quick testing script for the project."""
 import os
+import ssl
 from typing import Literal
 from unittest import mock
 
@@ -20,6 +21,8 @@ from two_plus_one import TwoPlusOneDataModule as TwoPlusOneDataModule
 from two_plus_one import TwoPlusOneUnetLightning as TwoPlusOneUnet
 from two_stream import TwoStreamCLI, TwoStreamDataModule, TwoStreamUnetLightning
 from utils.utils import ClassificationMode, LoadingMode, get_transforms
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 class TestTwoPlusOneCLI:
@@ -436,6 +439,132 @@ class TestTwoStreamCLI:
             self.default_train_args
             + self.default_resnet_args
             + self.greyscale_colour_mode
+            + self.fast_dev_run_args
+        )
+        self._run_with_args(args)
+
+
+class TestAttentionUnetCLI:
+    default_train_args = [
+        "fit",
+        "--trainer.precision=bf16-mixed",
+        "--model.classes=4",
+        "--config",
+        "./configs/residual_attention.yaml",
+        "--config",
+        "./configs/cine_tpo_resnet50.yaml",
+        "--data.num_workers=0",
+        "--config",
+        "./configs/no_checkpointing.yaml",
+        "--data.batch_size=1",
+    ]
+    default_frames = ["--model.num_frames=15"]
+    default_test_args = [
+        "test",
+        "--config",
+        "./configs/residual_attention.yaml",
+        "--config",
+        "./configs/cine_tpo_resnet50.yaml",
+        "--config",
+        "./configs/testing.yaml",
+        "--config",
+        "./configs/no_checkpointing.yaml",
+        "--data.batch_size=1",
+    ]
+    default_senet_args = ["--model.encoder_name=senet154"]
+    default_resnet_args = ["--model.encoder_name=resnet50"]
+    default_colour_mode = ["--image_loading_mode=RGB"]
+    greyscale_colour_mode = ["--config", "./configs/residual_attention_greyscale.yaml"]
+    flatten_conv_args = ["--model.flat_conv=True"]
+    fast_dev_run_args = ["--trainer.fast_dev_run=1"]
+    filename = "attention_unet.py"
+
+    def _run_with_args(self, args: list[str]):
+        with mock.patch("sys.argv", [self.filename] + args):
+            TwoPlusOneCLI(
+                TwoPlusOneUnet,
+                TwoPlusOneDataModule,
+                save_config_callback=None,
+                auto_configure_optimizers=False,
+            )
+
+    def test_quick_resnet_training(self):
+        """
+        Tests whether the ResNet50 (AttentionUnet) can train on a single batch.
+        """
+        args = (
+            self.default_train_args
+            + self.default_resnet_args
+            + self.default_frames
+            + self.fast_dev_run_args
+        )
+        self._run_with_args(args)
+
+    def test_quick_senet_training(self):
+        """
+        Tests whether the SENet154 (AttentionUnet) can validate on a single batch.
+        """
+        args = (
+            self.default_train_args
+            + self.default_senet_args
+            + self.default_frames
+            + self.fast_dev_run_args
+        )
+        self._run_with_args(args)
+
+    def test_quick_resnet_testing(self):
+        """
+        Tests whether the ResNet50 (AttentionUnet) can train on a single batch.
+        """
+        args = self.default_test_args + self.default_frames + self.fast_dev_run_args
+        self._run_with_args(args)
+
+    def test_quick_senet_testing(self):
+        """
+        Tests whether the SENet154 (AttentionUnet) can validate on a single batch.
+        """
+        args = self.default_test_args + self.default_frames + self.fast_dev_run_args
+        self._run_with_args(args)
+
+    def test_quick_resnet_rgb(self):
+        """
+        Tests whether the ResNet50 (AttentionUnet) can train on a single batch with RGB
+        images.
+        """
+        args = (
+            self.default_train_args
+            + self.default_resnet_args
+            + self.default_frames
+            + self.default_colour_mode
+            + self.fast_dev_run_args
+        )
+        self._run_with_args(args)
+
+    def test_quick_resnet_greyscale(self):
+        """
+        Tests whether the ResNet50 (AttentionUnet) can train on a single batch with
+        greyscale images.
+        """
+        args = (
+            self.default_train_args
+            + self.default_resnet_args
+            + self.default_frames
+            + self.greyscale_colour_mode
+            + self.fast_dev_run_args
+        )
+        self._run_with_args(args)
+
+    def test_flat_conv(self):
+        """
+        Tests whether the ResNet50 (AttentionUnet) can train with a flat temporal
+        convolutional layer.
+        """
+        args = (
+            self.default_train_args
+            + self.default_resnet_args
+            + ["--model.num_frames=30"]
+            + self.greyscale_colour_mode
+            + self.flatten_conv_args
             + self.fast_dev_run_args
         )
         self._run_with_args(args)
