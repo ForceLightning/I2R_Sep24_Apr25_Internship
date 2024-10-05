ForceLightning/I2R_Sep24_Apr25_Internship

commit db8c37dca6bcf0514903de2bd1e765b7b5ed2cbd

perf(data/loading): use `.all_frames()` to apply convert on load
Also add more transforms to the images.

dataset/dataset.py
@@ -242,11 +242,15 @@ class CineDataset(Dataset[tuple[torch.Tensor, torch.Tensor, str]]):
         with Image.open(
             os.path.join(self.img_dir, img_name), formats=["tiff"]
         ) as img_pil:
-            img_list = (
-                [img.convert("RGB") for img in ImageSequence.Iterator(img_pil)]
-                if self.loading_mode == LoadingMode.RGB
-                else [*ImageSequence.Iterator(img_pil)]
+            img_list = ImageSequence.all_frames(
+                img_pil,
+                lambda img: (
+                    img.convert("RGB")
+                    if self.loading_mode == LoadingMode.RGB
+                    else img.convert("L")
+                ),
             )
+
             img_list = self.transform_img(img_list)
             combined_imgs = tv_tensors.Video(default_collate(img_list))
 
@@ -357,11 +361,15 @@ class TwoPlusOneDataset(CineDataset):
         with Image.open(
             os.path.join(self.img_dir, img_name), formats=["tiff"]
         ) as img_pil:
-            img_list = (
-                [img.convert("RGB") for img in ImageSequence.Iterator(img_pil)]
-                if self.loading_mode == LoadingMode.RGB
-                else [*ImageSequence.Iterator(img_pil)]
+            img_list = ImageSequence.all_frames(
+                img_pil,
+                lambda img: (
+                    img.convert("RGB")
+                    if self.loading_mode == LoadingMode.RGB
+                    else img.convert("L")
+                ),
             )
+
             img_list = self.transform_img(img_list)
 
             combined_video = tv_tensors.Video(default_collate(img_list))
@@ -512,25 +520,22 @@ class TwoStreamDataset(Dataset[tuple[torch.Tensor, torch.Tensor, torch.Tensor, s
 
         # Convert LGE to RGB or Greyscale
         with Image.open(os.path.join(self.lge_dir, lge_name), formats=["png"]) as lge:
-            lge_pil = (
-                lge.convert("RGB")
-                if self.loading_mode == LoadingMode.RGB
-                else lge.convert("L")
-            )
+            out_lge = self.transform_img(lge.convert("L"))
 
         with Image.open(
             os.path.join(self.cine_dir, cine_name), formats=["tiff"]
         ) as cine:
-            cine_list = (
-                [img.convert("RGB") for img in ImageSequence.Iterator(img_pil)]
-                if self.loading_mode == LoadingMode.RGB
-                else [*ImageSequence.Iterator(cine)]
+            img_list = ImageSequence.all_frames(
+                cine,
+                lambda img: (
+                    img.convert("RGB")
+                    if self.loading_mode == LoadingMode.RGB
+                    else img.convert("L")
+                ),
             )
+            img_list = self.transform_img(img_list)
 
-            # Transform LGE and Cine together
-            out_lge, cine_list = self.transform_img(lge_pil, cine_list)
-
-        combined_cines = default_collate(cine_list)
+            combined_cines = tv_tensors.Video(default_collate(img_list))
 
         out_lge.squeeze()
 
@@ -645,10 +650,13 @@ class ResidualTwoPlusOneDataset(
         with Image.open(
             os.path.join(self.img_dir, img_name), formats=["tiff"]
         ) as img_pil:
-            img_list = (
-                [img.convert("RGB") for img in ImageSequence.Iterator(img_pil)]
-                if self.loading_mode == LoadingMode.RGB
-                else [*ImageSequence.Iterator(img_pil)]
+            img_list = ImageSequence.all_frames(
+                img_pil,
+                lambda img: (
+                    img.convert("RGB")
+                    if self.loading_mode == LoadingMode.RGB
+                    else img.convert("L")
+                ),
             )
             img_list = self.transform_img(img_list)
 

utils/utils.py
@@ -262,6 +262,8 @@ def get_transforms(
     # Randomly rotates +/- 180 deg and warps the image.
     transforms_together = Compose(
         [
+            v2.RandomHorizontalFlip(),
+            v2.RandomVerticalFlip(),
             v2.RandomRotation(
                 180.0,  # pyright: ignore[reportArgumentType]
                 v2.InterpolationMode.BILINEAR,
@@ -276,7 +278,7 @@ def get_transforms(
 
 
 def get_accumulate_grad_batches(batch_size: int):
-    if batch_size >= 4:
+    if batch_size >= 8:
         return 1
     else:
-        return 4 // batch_size
+        return 8 // batch_size

commit cbfe084e24bf2eb9abd2cdedb44189c7b8ed70a9

perf(dataloading): Move back to cv2 after regression in db8c37d

attention_unet.py
@@ -236,7 +236,9 @@ class ResidualAttentionUnetLightning(L.LightningModule):
         masks = masks.to(self.device.type).long()
 
         with torch.autocast(device_type=self.device.type):
-            masks_proba: torch.Tensor = self.model(images_input, res_input)  # pyright: ignore[reportCallIssue] # False positive
+            masks_proba: torch.Tensor = self.model(
+                images_input, res_input
+            )  # pyright: ignore[reportCallIssue] # False positive
 
             if self.dl_classification_mode == ClassificationMode.MULTILABEL_MODE:
                 # GUARD: Check that the sizes match.
@@ -324,7 +326,9 @@ class ResidualAttentionUnetLightning(L.LightningModule):
         res_input = res_images.to(self.device.type)
         masks = masks.to(self.device.type).long()
 
-        masks_proba: torch.Tensor = self.model(images_input, res_input)  # pyright: ignore[reportCallIssue] # False positive
+        masks_proba: torch.Tensor = self.model(
+            images_input, res_input
+        )  # pyright: ignore[reportCallIssue] # False positive
 
         if self.dl_classification_mode == ClassificationMode.MULTILABEL_MODE:
             # GUARD: Check that the sizes match.
@@ -487,7 +491,9 @@ class ResidualAttentionUnetLightning(L.LightningModule):
         res_input = res_images.to(self.device.type)
         masks = masks.to(self.device.type).long()
 
-        masks_proba: torch.Tensor = self.model(images_input, res_input)  # pyright: ignore[reportCallIssue]
+        masks_proba: torch.Tensor = self.model(
+            images_input, res_input
+        )  # pyright: ignore[reportCallIssue]
 
         if self.eval_classification_mode == ClassificationMode.MULTICLASS_MODE:
             masks_preds = masks_proba.argmax(dim=1)
@@ -604,12 +610,12 @@ class ResidualTwoPlusOneDataModule(L.LightningDataModule):
             self.val = test_dataset
             self.test = test_dataset
         else:
-            assert (
-                (idx := max(trainval_dataset.train_idxs)) < len(trainval_dataset)
+            assert (idx := max(trainval_dataset.train_idxs)) < len(
+                trainval_dataset
             ), f"Malformed training indices: {idx} for dataset of len: {len(trainval_dataset)}"
 
-            assert (
-                (idx := max(trainval_dataset.valid_idxs)) < len(trainval_dataset)
+            assert (idx := max(trainval_dataset.valid_idxs)) < len(
+                trainval_dataset
             ), f"Malformed training indices: {idx} for dataset of len: {len(trainval_dataset)}"
 
             valid_dataset = ResidualTwoPlusOneDataset(

dataset/dataset.py
@@ -7,6 +7,7 @@ import pickle
 import random
 from typing import Literal, Sequence, override
 
+import cv2
 import numpy as np
 import PIL.ImageSequence as ImageSequence
 import torch
@@ -243,22 +244,23 @@ class CineDataset(Dataset[tuple[torch.Tensor, torch.Tensor, str]]):
         img_name: str = self.img_list[index]
         mask_name: str = self.img_list[index].split(".")[0] + ".nii.png"
 
-        # PERF(PIL): This reduces the loading and transform time by 60% when compared
-        # to OpenCV.
-        with Image.open(
-            os.path.join(self.img_dir, img_name), formats=["tiff"]
-        ) as img_pil:
-            img_list = ImageSequence.all_frames(
-                img_pil,
-                lambda img: (
-                    img.convert("RGB")
-                    if self.loading_mode == LoadingMode.RGB
-                    else img.convert("L")
-                ),
-            )
+        # PERF: Initialise the output tensor ahead of time to reduce memory allocation
+        # time.
+        _, img_list = cv2.imreadmulti(
+            os.path.join(self.img_dir, img_name), flags=IMREAD_GRAYSCALE
+        )
+        combined_video = torch.empty((30, 224, 224), dtype=torch.uint8)
+        for i in range(30):
+            img = img_list[i]
+            img = cv2.resize(img, (224, 224))
+            combined_video[i, :, :] = torch.as_tensor(img)
 
-            img_list = self.transform_img(img_list)
-            combined_imgs = tv_tensors.Video(default_collate(img_list))
+        combined_video = combined_video.view(self.frames, 1, 224, 224)
+        if self.loading_mode == LoadingMode.RGB:
+            combined_video = combined_video.repeat(1, 3, 1, 1)
+
+        combined_video = tv_tensors.Video(combined_video)
+        combined_video = self.transform_img(combined_video)
 
         with Image.open(
             os.path.join(self.mask_dir, mask_name), formats=["png"]
@@ -288,11 +290,11 @@ class CineDataset(Dataset[tuple[torch.Tensor, torch.Tensor, str]]):
                     f"The mode {self.classification_mode.name} is not implemented"
                 )
 
-        out_video, out_mask = self.transform_together(combined_imgs, out_mask)
+        out_video, out_mask = self.transform_together(combined_video, out_mask)
         out_video = concatenate_imgs(self.frames, self.select_frame_method, out_video)
 
         f, c, h, w = out_video.shape
-        out_video = out_video.view(f * c, h, w)
+        out_video = out_video.reshape(f * c, h, w)
 
         return out_video, out_mask.squeeze().long(), img_name
 
@@ -365,23 +367,23 @@ class TwoPlusOneDataset(CineDataset):
         img_name = self.img_list[index]
         mask_name = self.img_list[index].split(".")[0] + ".nii.png"
 
-        # PERF(PIL): This reduces the loading and transform time by 60% when compared
-        # to OpenCV.
-        with Image.open(
-            os.path.join(self.img_dir, img_name), formats=["tiff"]
-        ) as img_pil:
-            img_list = ImageSequence.all_frames(
-                img_pil,
-                lambda img: (
-                    img.convert("RGB")
-                    if self.loading_mode == LoadingMode.RGB
-                    else img.convert("L")
-                ),
-            )
+        # PERF: Initialise the output tensor ahead of time to reduce memory allocation
+        # time.
+        _, img_list = cv2.imreadmulti(
+            os.path.join(self.img_dir, img_name), flags=IMREAD_GRAYSCALE
+        )
+        combined_video = torch.empty((30, 224, 224), dtype=torch.uint8)
+        for i in range(30):
+            img = img_list[i]
+            img = cv2.resize(img, (224, 224))
+            combined_video[i, :, :] = torch.as_tensor(img)
 
-            img_list = self.transform_img(img_list)
+        combined_video = combined_video.view(30, 1, 224, 224)
+        if self.loading_mode == LoadingMode.RGB:
+            combined_video = combined_video.repeat(1, 3, 1, 1)
 
-            combined_video = tv_tensors.Video(default_collate(img_list))
+        combined_video = tv_tensors.Video(combined_video)
+        combined_video = self.transform_img(combined_video)
 
         # Perform necessary operations on the mask
         with Image.open(
@@ -425,6 +427,8 @@ class TwoPlusOneDataset(CineDataset):
 
 
 class TwoStreamDataset(Dataset[tuple[torch.Tensor, torch.Tensor, torch.Tensor, str]]):
+    num_frames: int = 30
+
     def __init__(
         self,
         lge_dir: str,
@@ -524,27 +528,27 @@ class TwoStreamDataset(Dataset[tuple[torch.Tensor, torch.Tensor, torch.Tensor, s
         if not lge_name.endswith(".png"):
             raise ValueError("Invalid image type for file: {lge_name}")
 
-        # PERF(PIL): This reduces the loading and transform time by 60% when compared
-        # to OpenCV.
-
         # Convert LGE to RGB or Greyscale
         with Image.open(os.path.join(self.lge_dir, lge_name), formats=["png"]) as lge:
             out_lge = self.transform_img(lge.convert("L"))
 
-        with Image.open(
-            os.path.join(self.cine_dir, cine_name), formats=["tiff"]
-        ) as cine:
-            img_list = ImageSequence.all_frames(
-                cine,
-                lambda img: (
-                    img.convert("RGB")
-                    if self.loading_mode == LoadingMode.RGB
-                    else img.convert("L")
-                ),
-            )
-            img_list = self.transform_img(img_list)
+        # PERF: Initialise the output tensor ahead of time to reduce memory allocation
+        # time.
+        _, cine_list = cv2.imreadmulti(
+            os.path.join(self.cine_dir, cine_name), flags=IMREAD_GRAYSCALE
+        )
+        combined_cines = torch.empty((30, 224, 224), dtype=torch.uint8)
+        for i in range(30):
+            img = cine_list[i]
+            img = cv2.resize(img, (224, 224))
+            combined_cines[i, :, :] = torch.as_tensor(img)
 
-            combined_cines = tv_tensors.Video(default_collate(img_list))
+        combined_cines = combined_cines.view(self.num_frames, 1, 224, 224)
+        if self.loading_mode == LoadingMode.RGB:
+            combined_cines = combined_cines.repeat(1, 3, 1, 1)
+
+        combined_cines = tv_tensors.Video(combined_cines)
+        combined_cines = self.transform_img(combined_cines)
 
         out_lge.squeeze()
 
@@ -656,20 +660,23 @@ class ResidualTwoPlusOneDataset(
         img_name = self.img_list[index]
         mask_name = self.img_list[index].split(".")[0] + ".nii.png"
 
-        with Image.open(
-            os.path.join(self.img_dir, img_name), formats=["tiff"]
-        ) as img_pil:
-            img_list = ImageSequence.all_frames(
-                img_pil,
-                lambda img: (
-                    img.convert("RGB")
-                    if self.loading_mode == LoadingMode.RGB
-                    else img.convert("L")
-                ),
-            )
-            img_list = self.transform_img(img_list)
+        # PERF: Initialise the output tensor ahead of time to reduce memory allocation
+        # time.
+        _, img_list = cv2.imreadmulti(
+            os.path.join(self.img_dir, img_name), flags=IMREAD_GRAYSCALE
+        )
+        combined_video = torch.empty((30, 224, 224), dtype=torch.uint8)
+        for i in range(30):
+            img = img_list[i]
+            img = cv2.resize(img, (224, 224))
+            combined_video[i, :, :] = torch.as_tensor(img)
 
-            combined_video = tv_tensors.Video(default_collate(img_list))
+        combined_video = combined_video.view(30, 1, 224, 224)
+        if self.loading_mode == LoadingMode.RGB:
+            combined_video = combined_video.repeat(1, 3, 1, 1)
+
+        combined_video = tv_tensors.Video(combined_video)
+        combined_video = self.transform_img(combined_video)
 
         # Perform necessary operations on the mask
         with Image.open(
@@ -695,10 +702,10 @@ class ResidualTwoPlusOneDataset(
 
             case ClassificationMode.MULTICLASS_MODE:
                 pass
-            case _:
-                raise NotImplementedError(
-                    f"The mode {self.classification_mode.name} is not implemented"
-                )
+            # case _:
+            #     raise NotImplementedError(
+            #         f"The mode {self.classification_mode.name} is not implemented"
+            #     )
 
         combined_video, out_mask = self.transform_together(combined_video, out_mask)
         assert len(combined_video.shape) == 4, (