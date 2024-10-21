f5443324882ebae2015ce41b888e6f3d12ec394f
Christopher Kok <ForceLightning@users.noreply.github.com>
2024-10-21 16:24:57 +0800

    docs(sphinx): setup workflow for generating docs

---
+++ docs/source/conf.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "I2R Sep 2024-Apr 2025 Internship"
+copyright = "2024, Christopher Kok, Yu Yang"
+author = "Christopher Kok, Yu Yang"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+    "sphinx_rtd_theme",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "models.p3d", "tests"]
+
+language = "en"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# -- Options for Autodoc -----------------------------------------------------
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special_members": "__init__",
+    "undoc-members": False,
+    "private_members": False,
+    "show-inheritance": True,
+}
+
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.12/", None),
+    "torch": ("https://pytorch.org/docs/2.5/", None),
+    "numpy": ("https://numpy.org/doc/1.26/", None),
+    "segmentation_models_pytorch": ("https://smp.readthedocs.io/en/v0.3.4/", None),
+}
+
+sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../src/"))
---
+++ docs/source/index.rst
@@ -0,0 +1,32 @@
+.. I2R Sep 2024-Apr 2025 Internship documentation master file, created by
+   sphinx-quickstart on Mon Oct 21 15:25:44 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+I2R Sep 2024-Apr 2025 Internship documentation
+==============================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Modules
+=======
+* :mod:`models`
+* :mod:`dataset`
+* :mod:`metrics`
+* :mod:`utils`
+* :mod:`cli`
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
