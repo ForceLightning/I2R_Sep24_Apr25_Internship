ForceLightning/I2R_Sep24_Apr25_Internship

commit ddbdf1d551d5ac2f92fc226b862e7c7a50be54c8

fix(data/transforms): revert transforms back to 7236dee3 except for optical flow

src/attention_unet.py
@@ -131,7 +131,7 @@ class ResidualAttentionUnetLightning(L.LightningModule):
             encoder_name=encoder_name,
             encoder_depth=encoder_depth,
             encoder_weights=encoder_weights,
-            residual_mode=residual_mode,
+            # residual_mode=residual_mode,
             in_channels=in_channels,
             classes=classes,
             num_frames=num_frames,
@@ -676,7 +676,9 @@ class ResidualTwoPlusOneDataModule(L.LightningDataModule):
         # Handle color v. greyscale transforms.
 
         transforms_img, transforms_mask, transforms_together, transforms_resize = (
-            utils.get_transforms(self.loading_mode, self.augment)
+            ResidualTwoPlusOneDataset.get_default_transforms(
+                self.loading_mode, self.residual_mode, self.augment
+            )
         )
 
         trainval_dataset = ResidualTwoPlusOneDataset(

src/cine.py
@@ -594,8 +594,8 @@ class CineBaselineDataModule(L.LightningDataModule):
         trainval_img_dir = os.path.join(os.getcwd(), self.data_dir, "Cine")
         trainval_mask_dir = os.path.join(os.getcwd(), self.data_dir, "masks")
 
-        transforms_img, transforms_mask, transforms_together, transforms_resize = (
-            utils.get_transforms(self.loading_mode, self.augment)
+        transforms_img, transforms_mask, transforms_together = (
+            CineDataset.get_default_transforms(self.loading_mode, self.augment)
         )
 
         trainval_dataset = CineDataset(
@@ -604,7 +604,6 @@ class CineBaselineDataModule(L.LightningDataModule):
             indices_dir,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             transform_together=transforms_together,
             frames=self.frames,
             classification_mode=self.classification_mode,
@@ -622,7 +621,6 @@ class CineBaselineDataModule(L.LightningDataModule):
             indices_dir,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             mode="test",
             frames=self.frames,
             classification_mode=self.classification_mode,
@@ -649,7 +647,6 @@ class CineBaselineDataModule(L.LightningDataModule):
                 indices_dir,
                 transform_img=transforms_img,
                 transform_mask=transforms_mask,
-                transform_resize=transforms_resize,
                 classification_mode=self.classification_mode,
                 loading_mode=self.loading_mode,
                 combine_train_val=self.combine_train_val,

src/lge.py
@@ -70,8 +70,8 @@ class LGEBaselineDataModule(L.LightningDataModule):
         trainval_img_dir = os.path.join(os.getcwd(), self.data_dir, "LGE")
         trainval_mask_dir = os.path.join(os.getcwd(), self.data_dir, "masks")
 
-        transforms_img, transforms_mask, transforms_together, transforms_resize = (
-            utils.get_transforms(self.loading_mode, self.augment)
+        transforms_img, transforms_mask, transforms_together = (
+            LGEDataset.get_default_transforms(self.loading_mode, self.augment)
         )
 
         trainval_dataset = LGEDataset(
@@ -80,7 +80,6 @@ class LGEBaselineDataModule(L.LightningDataModule):
             indices_dir,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             transform_together=transforms_together,
             classification_mode=self.classification_mode,
             loading_mode=self.loading_mode,
@@ -97,7 +96,6 @@ class LGEBaselineDataModule(L.LightningDataModule):
             indices_dir,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             mode="test",
             classification_mode=self.classification_mode,
             loading_mode=self.loading_mode,
@@ -123,7 +121,6 @@ class LGEBaselineDataModule(L.LightningDataModule):
                 indices_dir,
                 transform_img=transforms_img,
                 transform_mask=transforms_mask,
-                transform_resize=transforms_resize,
                 classification_mode=self.classification_mode,
                 loading_mode=self.loading_mode,
                 combine_train_val=self.combine_train_val,

src/models/attention.py
@@ -478,7 +478,6 @@ class ResidualAttentionUnet(SegmentationModel):
         img_features_list: list[torch.Tensor] = []
         res_features_list: list[torch.Tensor] = []
 
-        # Go through by batch and get the results for each layer of the encoder.
         if isinstance(self.encoder, TSCSENetEncoder):
             for imgs, r_imgs in zip(regular_frames, residual_frames, strict=False):
                 self.check_input_shape(imgs)
@@ -491,6 +490,7 @@ class ResidualAttentionUnet(SegmentationModel):
             img_features_list = self.spatial_encoder(img_reshaped)
             res_features_list = self.residual_encoder(res_reshaped)
         else:
+            # Go through by batch and get the results for each layer of the encoder.
             for imgs, r_imgs in zip(regular_frames, residual_frames, strict=False):
                 self.check_input_shape(imgs)
                 self.check_input_shape(r_imgs)

src/two_plus_one.py
@@ -616,8 +616,8 @@ class TwoPlusOneDataModule(L.LightningDataModule):
         trainval_mask_dir = os.path.join(os.getcwd(), self.data_dir, "masks")
 
         # Get transforms for the CINE images, masks, and combined transforms.
-        transforms_img, transforms_mask, transforms_together, transforms_resize = (
-            utils.get_transforms(self.loading_mode, self.augment)
+        transforms_img, transforms_mask, transforms_together = (
+            TwoPlusOneDataset.get_default_transforms(self.loading_mode, self.augment)
         )
 
         trainval_dataset = TwoPlusOneDataset(
@@ -628,7 +628,6 @@ class TwoPlusOneDataModule(L.LightningDataModule):
             select_frame_method=self.select_frame_method,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             transform_together=transforms_together,
             classification_mode=self.classification_mode,
             loading_mode=self.loading_mode,
@@ -647,7 +646,6 @@ class TwoPlusOneDataModule(L.LightningDataModule):
             select_frame_method=self.select_frame_method,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             mode="test",
             classification_mode=self.classification_mode,
             loading_mode=self.loading_mode,
@@ -675,7 +673,6 @@ class TwoPlusOneDataModule(L.LightningDataModule):
                 select_frame_method=self.select_frame_method,
                 transform_img=transforms_img,
                 transform_mask=transforms_mask,
-                transform_resize=transforms_resize,
                 classification_mode=self.classification_mode,
                 loading_mode=self.loading_mode,
                 combine_train_val=self.combine_train_val,

src/two_stream.py
@@ -591,8 +591,8 @@ class TwoStreamDataModule(L.LightningDataModule):
         trainval_cine_dir = os.path.join(os.getcwd(), self.data_dir, "Cine")
         trainval_mask_dir = os.path.join(os.getcwd(), self.data_dir, "masks")
 
-        transforms_img, transforms_mask, transforms_together, transforms_resize = (
-            utils.get_transforms(self.loading_mode, self.augment)
+        transforms_img, transforms_mask, transforms_together = (
+            TwoStreamDataset.get_default_transforms(self.loading_mode, self.augment)
         )
 
         trainval_dataset = TwoStreamDataset(
@@ -602,7 +602,6 @@ class TwoStreamDataModule(L.LightningDataModule):
             indices_dir,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             transform_together=transforms_together,
             batch_size=self.batch_size,
             classification_mode=self.classification_mode,
@@ -622,7 +621,6 @@ class TwoStreamDataModule(L.LightningDataModule):
             indices_dir,
             transform_img=transforms_img,
             transform_mask=transforms_mask,
-            transform_resize=transforms_resize,
             batch_size=self.batch_size,
             mode="test",
             classification_mode=self.classification_mode,
@@ -650,7 +648,6 @@ class TwoStreamDataModule(L.LightningDataModule):
                 indices_dir,
                 transform_img=transforms_img,
                 transform_mask=transforms_mask,
-                transform_resize=transforms_resize,
                 batch_size=self.batch_size,
                 classification_mode=self.classification_mode,
                 loading_mode=self.loading_mode,

src/utils/utils.py
@@ -300,7 +300,7 @@ def configure_optimizers(module: L.LightningModule):
 
 def get_transforms(
     loading_mode: LoadingMode, augment: bool = False
-) -> tuple[Compose, Compose, Compose, Compose]:
+) -> tuple[Compose, Compose, Compose]:
     """Get default transformations for all datasets.
 
     The default implementation resizes the images to (224, 224), casts them to float32,
@@ -318,6 +318,7 @@ def get_transforms(
     transforms_img = Compose(
         [
             v2.ToImage(),
+            v2.Resize(224, antialias=True),
             v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             v2.Identity() if loading_mode == LoadingMode.RGB else v2.Grayscale(1),
@@ -328,6 +329,7 @@ def get_transforms(
     transforms_mask = Compose(
         [
             v2.ToImage(),
+            v2.Resize(224, antialias=True),
             v2.ToDtype(torch.long, scale=False),
         ]
     )
@@ -347,9 +349,7 @@ def get_transforms(
         else [v2.Identity()]
     )
 
-    transforms_resize = Compose([v2.Resize(224)])
-
-    return transforms_img, transforms_mask, transforms_together, transforms_resize
+    return transforms_img, transforms_mask, transforms_together
 
 
 def get_accumulate_grad_batches(batch_size: int):

commit d73cf51ffd420edfb48f57e111137eeaa877a326

fix(models/attention): ddbdf1d caused reduction mode to be skipped

src/attention_unet.py
@@ -131,7 +131,7 @@ class ResidualAttentionUnetLightning(L.LightningModule):
             encoder_name=encoder_name,
             encoder_depth=encoder_depth,
             encoder_weights=encoder_weights,
-            # residual_mode=residual_mode,
+            residual_mode=residual_mode,
             in_channels=in_channels,
             classes=classes,
             num_frames=num_frames,