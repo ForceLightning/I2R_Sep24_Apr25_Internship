9d14441828a03dc3b7581ef7a57949a3869819a7
Christopher Kok <ForceLightning@users.noreply.github.com>
2024-12-09 15:56:25 +0800

    wip: flanet debugging

--- src/dataset/flanet.py
+++ src/dataset/flanet.py
@@ -7,7 +7,11 @@ from __future__ import annotations
 
 # Standard Library
 import os
-from typing import Literal, override
+from typing import Literal, Optional, Sequence, override
+
+# Scientific Libraries
+import numpy as np
+from numpy import typing as npt
 
 # Image Libraries
 import cv2
@@ -16,6 +20,7 @@ from PIL import Image
 
 # PyTorch
 import torch
+from torch import Tensor
 from torch.nn import functional as F
 from torch.nn.common_types import _size_2_t
 from torch.utils.data import Dataset
@@ -23,9 +28,6 @@ from torchvision import tv_tensors
 from torchvision.ops import masks_to_boxes
 from torchvision.transforms import v2
 from torchvision.transforms.v2 import Compose
-
-# State-of-the-Art (SOTA) code
-from thirdparty.fla_net.Code.utils.video_dataloader3 import HeatmapGenerator
 
 # First party imports
 from dataset.dataset import (
@@ -37,8 +39,60 @@ from dataset.dataset import (
 from utils.types import ClassificationMode, LoadingMode
 
 
+class HeatmapGenerator:
+    """Heatmap generator for FLA-Net."""
+
+    def __init__(
+        self, output_res: int, num_joints: int, sigma: Optional[int] = None
+    ) -> None:
+        """Initialise the Heatmap generator."""
+        self.output_res = output_res
+        self.num_joints = num_joints
+        if sigma is None:
+            self.sigma = self.output_res / 64
+        else:
+            self.sigma = sigma
+
+        size = 6 * self.sigma + 3
+        x = np.arange(0, size, 1, float)
+        y = x[:, np.newaxis]
+        x0, y0 = 3 * self.sigma + 1, 3 * self.sigma + 1
+        self.g = np.exp(-((x - x0) ** 2 + (y - y0) ** 2) / (2 * self.sigma**2))
+
+    def __call__(
+        self, joints: Sequence[tuple[int, int, int]]
+    ) -> npt.NDArray[np.float32]:
+        """Generate heatmap."""
+        hms = np.zeros(
+            (self.num_joints, self.output_res, self.output_res), dtype=np.float32
+        )
+        for idx, pt in enumerate(joints):
+            if pt[2] > 0:
+                x, y = int(pt[0]), int(pt[1])
+                if any((x < 0, y < 0, x >= self.output_res, y >= self.output_res)):
+                    continue
+
+                ul = int(np.round(x - 3 * self.sigma - 1)), int(
+                    np.round(y - 3 * self.sigma - 1)
+                )
+                br = int(np.round(x + 3 * self.sigma + 2)), int(
+                    np.round(y + 3 * self.sigma + 2)
+                )
+
+                c, d = max(0, -ul[0]), min(br[0], self.output_res) - ul[0]
+                a, b = max(0, -ul[1]), min(br[1], self.output_res) - ul[1]
+
+                cc, dd = max(0, ul[0]), min(br[0], self.output_res)
+                aa, bb = max(0, ul[1]), min(br[1], self.output_res)
+
+                hms[idx, aa:bb, cc:dd] = np.maximum(
+                    hms[idx, aa:bb, cc:dd], self.g[a:b, c:d]
+                )
+        return hms
+
+
 class FLANetDataset(
-    Dataset[tuple[torch.Tensor, torch.Tensor, torch.Tensor, str]],
+    Dataset[tuple[Tensor, Tensor, Tensor, str]],
     DefaultTransformsMixin,
     DefaultDatasetProtocol,
 ):
@@ -110,9 +164,7 @@ class FLANetDataset(
         self.heatmap = HeatmapGenerator(224, 1, 10)
 
     @override
-    def __getitem__(
-        self, index
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, str]:
+    def __getitem__(self, index) -> tuple[Tensor, Tensor, Tensor, str]:
 
         # Define Cine file name
         img_name: str = self.img_list[index]
@@ -156,8 +208,8 @@ class FLANetDataset(
         )
         bbox = masks_to_boxes(class_mask).squeeze()
         h, w = bbox[3] - bbox[1], bbox[2] - bbox[0]
-        centre_x, centre_y = bbox[0] + w // 2, bbox[1] + h // 2
-        heatmap = self.heatmap([[centre_x, centre_y, 1]])
+        centre_x, centre_y = int(bbox[0] + w // 2), int(bbox[1] + h // 2)
+        heatmap = self.heatmap([(centre_x, centre_y, 1)])
         heatmap = self.transform_heatmap(heatmap)
 
         match self.classification_mode:
@@ -165,16 +217,7 @@ class FLANetDataset(
                 # NOTE: This turns the problem into a multilabel segmentation problem.
                 # As label_3 ⊂ label_2 and label_2 ⊂ label_1, we need to essentially apply
                 # bitwise or operations to adhere to those conditions.
-                lab_mask_one_hot = F.one_hot(
-                    out_mask.squeeze(), num_classes=4
-                )  # H x W x C
-                lab_mask_one_hot[:, :, 2] = lab_mask_one_hot[:, :, 2].bitwise_or(
-                    lab_mask_one_hot[:, :, 3]
-                )
-                lab_mask_one_hot[:, :, 1] = lab_mask_one_hot[:, :, 1].bitwise_or(
-                    lab_mask_one_hot[:, :, 2]
-                )
-                out_mask = lab_mask_one_hot.bool().permute(-1, 0, 1)
+                out_mask = out_mask_one_hot
 
             case ClassificationMode.MULTICLASS_MODE:
                 pass
