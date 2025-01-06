9c4d2ae8b7b535c35c016be210db04900c075d86
Christopher Kok <ForceLightning@users.noreply.github.com>
2025-01-07 02:04:09 +0800

    TODO: Fix UR estimation for binary segmentation case

--- src/models/attention/urr/utils.py
+++ src/models/attention/urr/utils.py
@@ -19,6 +19,7 @@ def calc_uncertainty(score: Tensor) -> Tensor:
 
     """
     # seg shape: bs, obj_n, h, w
+    # TODO: Fix for case where binary classification mode is used.
     score_top, _ = score.topk(k=2, dim=1)
     uncertainty = score_top[:, 0] / (score_top[:, 1] + 1e-8)  # bs, h, w
     uncertainty = torch.exp(1 - uncertainty).unsqueeze(1)  # bs, 1, h, w
