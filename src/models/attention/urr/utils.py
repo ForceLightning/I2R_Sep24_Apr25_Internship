e17344f1a376ed1324ed5e0b5fbeb64c696293a4
Christopher Kok <ForceLightning@users.noreply.github.com>
2025-01-22 14:22:12 +0800

    chore(logging): debug log instead of print

--- src/models/attention/urr/utils.py
+++ src/models/attention/urr/utils.py
@@ -1,11 +1,14 @@
 """Utility functions for URR-augmented attention model."""
 
 # Standard Library
+import logging
 from enum import Enum, auto
 
 # PyTorch
 import torch
 from torch import Tensor
+
+logger = logging.getLogger(__name__)
 
 
 def calc_uncertainty(score: Tensor) -> Tensor:
@@ -23,7 +26,7 @@ def calc_uncertainty(score: Tensor) -> Tensor:
         assert score.dim() == 4
         if score.shape[-3] == 1:
             score_top = torch.cat((torch.ones_like(score) - score, score), dim=1)
-            print(score_top.shape)
+            logger.debug("score_top.shape: %s", str(score_top.shape))
         else:
             score_top, _ = score.topk(k=2, dim=1)
         uncertainty = score_top[:, 0] / (score_top[:, 1] + 1e-8)  # bs, h, w
